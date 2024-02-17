0cbb2ab93a236ea9733e8018201f4470d0ee0f7a
LUCAS MORAES ALVES <LMOALVES@bbmapfre.com.br>
2024-02-17 20:38:14 -0300

    operators

---
+++ main_operators.py
@@ -0,0 +1,35 @@
+#operação matemática em python
+
+'''
+calculo = 2 + 2 * 3
+
+print(calculo)
+'''
+
+#parenteses
+#exponenciais
+#mutiplicação e disvisão
+#adição e subtração
+
+# Operadores de comparação
+    # == Equal
+    # != Not equal
+    # > Greater than
+    # < Less than
+    # >= Greater than or equal to
+    # <= Less than or equal to 
+
+
+#Variables
+
+'''
+operadores = 'banana' == 'banana'
+
+print(operadores)
+'''
+
+# Operadores de Atribuição
+
+# x = x + 5 é igual a x (+,-,*,/ e %)= 5
+# A "%" é a quantidade de vezes que um número entra dentro de outro número
+
