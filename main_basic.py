bdfe79acde2b7b3fb02c746b86aa8571865a7309
LUCAS MORAES ALVES <LMOALVES@bbmapfre.com.br>
2024-02-17 11:39:18 -0300

    formated string

---
+++ main_basic.py
@@ -0,0 +1,66 @@
+''' #Print test no console
+
+print('teste')
+
+#print variable
+
+x = 2
+
+print(x)
+
+'''
+'''
+#variables
+x = str(3)
+y = int(4)
+z = float(5)
+
+print(x + x)
+print(y + y)
+print(z + z)
+'''
+
+'''
+#O Lucas tem 28 anos e mora em barueri
+
+nome = 'Lucas'
+idade = 28
+idade = str(idade)
+cidade = 'Barueri' 
+
+print('O ' + nome + ' com a idade de ' + idade + ' e mora na cidade de '  + cidade + '.')
+'''
+
+'''
+#adicionando input
+nome = input('Qual o seu nome: ')
+nasc = input('Qual é o seu ano de nascimento: ')
+ano = int(2024) - int(nasc)
+ano = str(ano)
+
+print('O seu nome é: ' + nome + ' e você tem ' + ano + ' anos de idade.')
+'''
+
+'''
+#usando slice
+valor = 99.75
+valor = str(valor)
+#index  01234
+
+print('centavos ' + valor[3:5])
+'''
+
+'''
+#Formated String
+nome = 'Marcos'
+sobrenome = 'Silva'
+profissao = 'programador'
+
+texto = 'O ' + nome + ' ' + sobrenome + ' é um excelente ' + profissao + '.'
+
+print(texto)
+
+texto2 = f'O {nome} {sobrenome} é um excelente {profissao}.'
+
+print(texto2)
+'''
