74193d93e077723bd7a15d51274574e24f569842
LUCAS MORAES ALVES <LMOALVES@bbmapfre.com.br>
2024-02-17 11:06:05 -0300

    pequenas mudanças

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-#Print test no console
+''' #Print test no console
 
 print('teste')
+
+#print variable
+
+x = 2
+
+print(x)
+
+'''
+'''
+#variables
+x = str(3)
+y = int(4)
+z = float(5)
+
+print(x + x)
+print(y + y)
+print(z + z)
+'''
+
+
+#O Lucas tem 28 anos e mora em barueri
+
+nome = 'Lucas'
+idade = 28
+idade = str(idade)
+cidade = 'Barueri' 
+
+print('O ' + nome + ' com a idade de ' + idade + ' e mora na cidade de '  + cidade + '.')
+
+
+
+
