410e6da08ff9c53316854314512b45f2d85c7417
LUCAS MORAES ALVES <LMOALVES@bbmapfre.com.br>
2024-02-17 11:32:38 -0300

    slice

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ print(y + y)
 print(z + z)
 '''
 
-
+'''
 #O Lucas tem 28 anos e mora em barueri
 
 nome = 'Lucas'
@@ -29,7 +29,23 @@ idade = str(idade)
 cidade = 'Barueri' 
 
 print('O ' + nome + ' com a idade de ' + idade + ' e mora na cidade de '  + cidade + '.')
+'''
 
+'''
+#adicionando input
+nome = input('Qual o seu nome: ')
+nasc = input('Qual é o seu ano de nascimento: ')
+ano = int(2024) - int(nasc)
+ano = str(ano)
 
+print('O seu nome é: ' + nome + ' e você tem ' + ano + ' anos de idade.')
+'''
 
+'''
+#usando slice
+valor = 99.75
+valor = str(valor)
+#index  01234
 
+print('centavos ' + valor[3:5])
+'''
