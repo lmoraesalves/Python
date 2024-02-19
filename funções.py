5bbfb4808594b41dc9488682d5f4e9beef0ac0fe
LUCAS MORAES ALVES <LMOALVES@bbmapfre.com.br>
2024-02-19 12:45:21 -0300

    módulos

--- funções.py
+++ funções.py
@@ -42,9 +42,50 @@
 
 
 
-#Argumento default e non-default
-def var_boasvindas(nome, quantidade=6):
-    print(f'Olá {nome}')
-    print(f'Temos {str(quantidade)} de laptops em estoque.')
+# #Argumento default e non-default
+# def var_boasvindas(nome, quantidade=6):
+#     print(f'Olá {nome}')
+#     print(f'Temos {str(quantidade)} de laptops em estoque.')
 
-var_boasvindas('Lucas')
+# var_boasvindas('Lucas')
+
+# def cliente1(nome):
+#     print(f'Olá {nome}')
+
+
+# def cliente2(nome):
+#     return f'Olá {nome}'
+
+
+# x = cliente1('Lucas')
+# y = cliente2('Anna')
+
+# print(y)
+# print(x)
+
+#criar função que soma vários números
+
+# def soma(*var_numeros):
+#     var_result = 0
+#     for var_num in var_numeros:
+#         var_result += var_num
+#     return var_result
+
+
+# var_x = soma(2, 3, 4, 7)
+# print(var_x)
+
+# #Vários argumentos xargs nomeando parâmetros
+
+# def agencia(**carro):
+#     return carro
+
+# print(agencia(marca='Gol', cor='Branco', motor=1.0, placa=1234))
+
+
+#importando os módulos
+#Qual o número fatorial de 4
+
+import math
+
+print(math.factorial(57))
