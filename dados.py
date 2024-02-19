1cb9e6a74fb9df789e7ecc2612971240085645f2
LUCAS MORAES ALVES <LMOALVES@bbmapfre.com.br>
2024-02-19 17:01:59 -0300

    100

--- dados.py
+++ dados.py
@@ -61,11 +61,81 @@
 
 # print(var_numeros)
 
-# Listas dentro de listas
+# # Listas dentro de listas
 
-var_itens = [
-    ['Item 1', 'Item 2'],
-    ['Item 3', 'Item 4']
-]
+# var_numero = [1, 2, 3, 4]
+# print(var_numero[1])
 
-print(var_itens[0][1])
+# var_itens = [
+#     ['Item 1', 'Item 2'],
+#     ['Item 3', 'Item 4']
+# ]
+
+# print(var_itens[0][1])
+
+# # Extrair variáveis de Listas *Unpacking list*
+#     # Armazenar mais de uma infromação em variáveis
+#     # Manter a sequencia dos dados em uma variavel
+
+# var_produtos = [
+#     'Arroz',    #Index0
+#     'Feijão',   #Index1
+#     'Laranja',  #Index2
+#     'Banana'    #Index3
+# ]
+
+# var_item1, var_item2, var_item3, *var_outros = var_produtos
+
+# print(var_item1)
+# print(var_item2)
+# print(var_item3)
+# print(var_outros)
+
+# #For loop dentro de uma lista
+
+# var_valores = [10, 20, 40, 80, 160]
+
+# for var_x in var_valores:
+#     print(f'O valor final do seu produto é: R${var_x}')
+
+# # Verificando itens em uma lista
+
+# var_cor_client = input('Digite a cor da tinta: ')
+# # var_cor_client = var_cor_client.lower()
+
+# var_cor = [
+#     'amarelo',
+#     'verde',
+#     'azul',
+#     'vermelho'
+#     ]
+
+# if var_cor_client.lower() in var_cor:
+#     print(f'A cor {var_cor_client.lower()} está disponível.')
+# else:
+#     print(f'A cor {var_cor_client.lower()} não está disponível.')
+
+# # Agregando duas listas com o ZIP
+
+# var_cor = [
+#     'amarelo',
+#     'verde',
+#     'azul',
+#     'vermelho'
+#     ]
+
+# var_valor = [
+#     10, 20, 30, 40
+#     ]
+
+# var_consolidado = zip(var_cor, var_valor)
+
+# print(list(var_consolidado))
+
+# # Criar uma lista de frutas a partir do input fornecido pelo User
+
+# var_frutas_user = input('Por gentileza, digite as frutas que deseja comprar, separados por vírgula: ')
+# var_frutas_list = var_frutas_user.split(', ')
+
+
+# print(var_frutas_list)
